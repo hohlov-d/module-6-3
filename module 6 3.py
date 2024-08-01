b2716def5ac83e4dd5385a4a2b84afb3b69da625
hohlov-d <hohlov_d.o@mail.ru>
2024-08-02 03:47:28 +0500

    Add files via upload

--- module 6 3.py
+++ module 6 3.py
@@ -1,28 +1,29 @@
 class Horse:
     def __init__(self):
         self.x_distance = 0
-        self.sound = 'Frrr'
+        self.sound = 'Frr'
+        super().__init__()
+
 
     def run(self, dx):
         self.x_distance += dx
+
 
 class Eagle:
     def __init__(self):
         self.y_distance = 0
         self.sound = 'I train, eat, sleep, and repeat'
+        super().__init__()
 
     def fly(self, dy):
         self.y_distance += dy
 
+
 class Pegasus(Horse, Eagle):
-    def __init__(self):
-        super().__init__()
-        Horse.__init__(self)
-        Eagle.__init__(self)
 
     def move(self, dx, dy):
-        self.run(dx)
-        self.fly(dy)
+        super().run(dx)
+        super().fly(dy)
 
     def get_pos(self):
         return self.x_distance, self.y_distance
@@ -40,5 +41,3 @@ p1.move(-5, 20)
 print(p1.get_pos())
 
 p1.voice()
-
-
